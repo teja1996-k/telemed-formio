d738aabfe14683fe0466aa3535305d41ec9a23b4
tejaswini.k <tejaswini.k@techsophy.com>
2020-04-19 10:43:24 +0530

    working

--- src/app/wizard/wizard.component.ts
+++ src/app/wizard/wizard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Renderer2, AfterViewChecked } from '@angular/core';
 import wizard from '../../assets/wizard.json';
 import form1 from '../../assets/form1.json';
 import form2 from '../../assets/form2.json';
@@ -10,7 +10,7 @@ import { viewClassName } from '@angular/compiler';
   styleUrls: ['./wizard.component.scss'],
   encapsulation:ViewEncapsulation.None
 })
-export class WizardComponent implements OnInit {
+export class WizardComponent implements OnInit,AfterViewChecked {
   public wizardForm:any=wizard
   public renderOptions = {
     buttonSettings: {
@@ -18,14 +18,49 @@ export class WizardComponent implements OnInit {
       showNext: false,
       showPrevious: false,
       showSubmit: false
-    }
+    },
+    noAlerts: true
   };
+  public options={
+    options : {
+    noAlerts: true
+    }
+  }
+  public paging:any;
+  public count:number=0;
 
-  constructor() { }
+  constructor(
+    private renderer : Renderer2,
+  ) { }
+  ngAfterViewChecked(){
+    if(this.count<1){
+      this.count=this.count+1;
+      
+    this.paging = document.getElementsByClassName('pagination')
+    console.log(this.paging);
+    const span=this.renderer.createElement('span')
+    var img=this.renderer.createElement('img')
+    img.src="../../assets/thumbay-logo.jpg"
+    img.height=55
+    this.renderer.setStyle(img,'margin-top','-10px')
+    this.renderer.appendChild(span,img);
+    this.renderer.appendChild(this.paging[0],span);
+    this.renderer.insertBefore(this.paging[0],span,this.paging[0].firstChild);
+    // this.renderer.appendChild(document.body,this.paging);
+    }
+    else{
+      console.log(this.count)
+    }
+  
+
+  }
 
   ngOnInit(): void {
     this.wizardForm['components'][0]['components']=form1['components'];
     this.wizardForm['components'][1]['components']=form2['components'];
+  }
+  displayDialog(){
+    console.log("method called")
   }
 
 }
